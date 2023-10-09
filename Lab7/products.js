7ffbfbdfc55fbb3ce1d6aaa0b5515e34d135b781
aaronk1m <aaronkimhawaii2002@gmail.com>
2023-10-08 20:09:40 -1000

    retry

--- Lab7/products.js
+++ Lab7/products.js
@@ -20,108 +20,119 @@ let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackber
 
 console.log("Product variables loaded...");
 
-//Exercise 1.1
-console.log("Exercise 1.1");
-let num_products = 5;
+console.log();
+console.log("exercise 1.1");
+let num_products=5;
 let productCount = 1;
-while (productCount <= num_products){
+while(productCount<=num_products){
+    console.log(productCount); 
+    productCount++;
+}
+
+
+console.log();
+console.log("Exercise 1.2");
+productCount = 1;
+
+while(productCount<= num_products){
+    console.log(productCount+". "+eval('name'+productCount));
+    productCount++;
+}
+console.log("That's all we have!");
+
+console.log();
+console.log("Exercise 1.3");
+productCount = 1;
+
+while(productCount<= (num_products/2)){
+    console.log(productCount+". "+eval('name'+productCount));
+    productCount++;
+}
+console.log("That's all we have!");
+
+console.log();
+console.log("Exercise 2.1");
+productCount =1;
+
+while(productCount<=num_products){
+
+    if(productCount>num_products/2){
+        console.log("That's enough");
+        break;
+    }
     console.log(productCount);
     productCount++;
 }
 
-//Exercise 1.2
-console.log();
-console.log("Exercise 1.2");
-productCount = 1
-while (productCount <= num_products){
-    console.log(productCount +'. '+eval('name' + productCount));
-    productCount++;
-}
-
-//Exercise 1.3
-console.log("Exercise 1.3");
-productCount = 1
-while (productCount <= num_products/2){
-    console.log(productCount +'. '+eval('name' + productCount));
-    productCount++;
-}
-
-//Exercise 2.1
-console.log();
-console.log("Exercise 2.1");
-productCount = 1;
-while (productCount <= num_products){
-    if (productCount >= num_products/2){
-        console.log("That's enough");
-        break;
-    }
-        console.log(productCount);
-        productCount++;
-    }
-    
-//Exercise 2.2
 console.log();
 console.log("Exercise 2.2");
-productCount = 1;
+productCount =1;
 let lowerBound = num_products*.25;
 let upperBound = num_products*.75;
-while (productCount <= num_products){
-    if (productCount >= lowerBound && productCount <= upperBound){
-        console.log (eval('name' + productCount) + ' is sold out!!');
-    } else {
-    console.log(productCount +'. '+eval('name' + productCount));
+while(productCount<=num_products){
+    if(productCount>=lowerBound && productCount<=upperBound){
+        console.log(eval('name'+productCount) + ' is sold out!!');
+    }else{
+    console.log(productCount+'. '+eval('name'+productCount));
+    
     }
     productCount++;
 }
 
-//Exercise 2.3
 console.log();
 console.log("Exercise 2.3");
-productCount = 1;
-while (productCount <= num_products){
-    if (productCount >= num_products/2){
-        console.log ("Don't ask for anything else");
-        //process.exit ();
+productCount =1;
+while(productCount<=num_products){
+    if(productCount >= num_products/2)
+    {
+        console.log("Don't ask for anything else");
+        //process.exit(); (To let other things work)
     }
-    if (productCount >= lowerBound && productCount <= upperBound){
-        console.log (eval('name' + productCount) + ' is sold out!!');
-    } else {
-    console.log(productCount +'. '+eval('name' + productCount));
+    if(productCount>=lowerBound && productCount<=upperBound){
+        console.log(eval('name'+productCount) + ' is sold out!!');
+    }else{
+    console.log(productCount+'. '+eval('name'+productCount));
+    
     }
     productCount++;
 }
 
-//Exercise 3.1a
 console.log();
 console.log("Exercise 3.1a");
-productCount = 1;
-for (let i=1; i<= num_products; i++) {
-    if (i >= lowerBound && i <= upperBound){
-        console.log (eval('name' + i) + ' is sold out!!');
-    } else {
-    console.log(i +'. '+eval('name' + i));
+productCount =1;
+
+for(let i=1; i<= num_products; i++){
+
+    if(i>=lowerBound && i<=upperBound){
+        console.log(eval('name'+i) + ' is sold out!!');
+    }else{
+    console.log(i+'. '+eval('name'+i));
+    
     }
 }
 
-//Exercise 3.1b
 console.log();
 console.log("Exercise 3.1b");
-for (let i=1; eval("typeof name" + i) != 'undefined'; i++) {
-    if (i >= lowerBound && i <= upperBound) {
-        console.log (eval('name' + i) + ' is sold out!!');
-    } else {
-    console.log(i +'. '+eval('name' + i));
+
+for(let i=1; eval("typeof name"+i) != 'undefined'; i++){
+    
+    if(i>=lowerBound && i<=upperBound){
+        console.log(eval('name'+i) + ' is sold out!!');
+    }else{
+    console.log(i+'. '+eval('name'+i));
+    
     }
 }
 
-//Exercise 4.2
-//Creating table header
-document.write('<table border="1">');
-document.write('<tr><th>Number</th><th>Item</th></tr>');
-//Loop to output rows
-for (let i=1; eval("typeof name" + i) != 'undefined'; i++) {
-    document.write(`<tr><td>${i}</td><td>${eval("name" +i)}</td></tr>`);
+document.write('<table border="1",id="invoiceTable"><th>Number</th><th>Item</th>');
+productCount = 1;
+
+while(productCount<= num_products){
+    
+    document.write("<tr>");
+    document.write(`<td>${productCount}</td><td>${eval('name' + productCount)}</td>`);
+    document.write("</tr>");
+    productCount++;
 }
 
-//Close the table
-document.write('</table>');
+document.write('</table></tbody>');
